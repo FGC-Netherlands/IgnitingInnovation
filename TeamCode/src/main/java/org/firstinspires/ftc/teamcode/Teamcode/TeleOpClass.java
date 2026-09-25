be67ee24e0d53357327c5288cd6d386dab976e48
58147768 <jspr.m.dejong@gmail.com>
2026-09-25 13:21:49 +0200

    error gone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teamcode/TeleOpClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teamcode/TeleOpClass.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Teamcode;
+package org.firstinspires.ftc.teamcode.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
